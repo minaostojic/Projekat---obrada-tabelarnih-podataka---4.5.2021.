eff43a72c07c8320c240c944797c51bdd0a3da32
𝕸𝖎𝖓𝖆  𝕸 <>
2021-05-04 12:55:33 +0000

    Unos podataka u matricu

--- main.cs
+++ main.cs
@@ -1,7 +1,32 @@
 using System;
+using System.IO;
+using System.Globalization;
 
 class MainClass {
+  static void Ucitavanje_podataka (ref string[,] matrica)
+  {
+    if (File.Exists("ulazni_podaci.csv"))
+    {
+      StreamReader podaci = new StreamReader("ulazni_podaci.csv");
+      //string[,] matrica = new string[1000,6];
+      int brojac=0;
+      string s = podaci.ReadLine();
+      while (!podaci.EndOfStream)      
+      {
+        s = podaci.ReadLine();
+        string[] elementi = s.Split(";");
+        for (int i=0; i<6; i++)
+        {
+          matrica[brojac,i] = elementi[i];
+        }
+        brojac++;
+      }
+      podaci.Close();
+    }
+    else Console.Error.WriteLine("Greska! Ne postoji datoteka ulazni_podaci");
+  }
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    string[,] podaci_matrica = new string[1000,6];
+    Ucitavanje_podataka(ref podaci_matrica);
   }
 }
